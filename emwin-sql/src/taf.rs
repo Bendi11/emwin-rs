de76aba667ff4c7fc415b6e8c688b0f6f1e11555
Bendi11 <bkliebmann@gmail.com>
2022-11-08 17:53:01 -0800

    Removed 'db' module from emwind

--- emwin-sql/src/taf.rs
+++ emwin-sql/src/taf.rs
@@ -8,6 +8,6 @@ impl<E: for<'c> Executor<'c>> EmwinSqlContext<E> {
     /// Insert a TAF report item into the database connection, returning the ID of the inserted
     /// row of the `weather.taf_item` table
     pub async fn insert_taf(&self, taf: &TAFReportItem) -> Result<u64, sqlx::Error> {
-        
+        todo!();
     }
 }
