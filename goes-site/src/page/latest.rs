0b2381dbc586aa7f4e2e6b435832d7b88cebe3a3
Bendi11 <bkliebmann@gmail.com>
2022-12-07 10:23:22 -0800

    Replaced duplicated SQL query with call to img_single_ep

--- goes-site/src/page/latest.rs
+++ goes-site/src/page/latest.rs
@@ -9,29 +9,20 @@ use sqlx::{MySqlPool, Row};
 
 use crate::map_path;
 
+use super::search::{img_single_ep, QueryForm, TimeQuery};
+
 pub fn latest_scope() -> Scope {
     web::scope("/latest")
         //.service(web::resource("/fd.html").to(latest_fd))
         .service(web::resource("/fd_fc.jpg").to(latest_fd_fc))
 }
 
-async fn latest_fd_fc(sql: Data<MySqlPool>, cfg: Data<Config>) -> Result<impl Responder> {
-    let fd = sqlx::query(
-r#"
-SELECT (file_name)
-FROM goesimg.files
-WHERE start_dt=(SELECT max(start_dt) FROM goesimg.files WHERE sector='FULL_DISK' AND channel='FULL_COLOR') AND sector='FULL_DISK' AND channel='FULL_COLOR';
-"#
-    )
-        .fetch_one(sql.get_ref())
-        .await
-        .map_err(|e| error::ErrorBadRequest(e))
-        .and_then(|v| v
-            .try_get::<&str, _>(0)
-            .map_err(|e| error::ErrorBadRequest(e))
-            .and_then(map_path(cfg.get_ref()))
-            .map(std::path::Path::to_owned)
-        )?;
-
-    Ok(NamedFile::open(cfg.img_dir.join(fd))?)
+async fn latest_fd_fc(sql: Data<MySqlPool>) -> Result<impl Responder> {
+    img_single_ep(sql, &QueryForm {
+        time: TimeQuery::Latest,
+        sector: Some("FULL_DISK".to_owned()),
+        channel: Some("FULL_COLOR".to_owned()),
+        acronym: None,
+        satellite: None,
+    }).await
 }
