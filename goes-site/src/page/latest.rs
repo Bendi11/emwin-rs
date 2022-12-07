9b813ba57a540c0cf8bcaa50c657851dafebf547
Bendi11 <bkliebmann@gmail.com>
2022-12-07 10:17:55 -0800

    Removed unused code, redundant string conversions for errors

--- goes-site/src/page/latest.rs
+++ goes-site/src/page/latest.rs
@@ -25,58 +25,13 @@ WHERE start_dt=(SELECT max(start_dt) FROM goesimg.files WHERE sector='FULL_DISK'
     )
         .fetch_one(sql.get_ref())
         .await
-        .map_err(|e| error::ErrorBadRequest(e.to_string()))
+        .map_err(|e| error::ErrorBadRequest(e))
         .and_then(|v| v
             .try_get::<&str, _>(0)
-            .map_err(|e| error::ErrorBadRequest(e.to_string()))
+            .map_err(|e| error::ErrorBadRequest(e))
             .and_then(map_path(cfg.get_ref()))
             .map(std::path::Path::to_owned)
         )?;
 
     Ok(NamedFile::open(cfg.img_dir.join(fd))?)
 }
-
-/*async fn latest_fd(sql: Data<MySqlPool>, cfg: Data<Config>) -> Result<Latest> {
-    let (fd_fc, fd_fc_dt)  =  sqlx::query(
-r#"
-SELECT file_name, start_dt
-FROM goesimg.files
-WHERE start_dt=(SELECT max(start_dt) FROM goesimg.files WHERE sector='FULL_DISK' AND channel='FULL_COLOR') AND sector='FULL_DISK' AND channel='FULL_COLOR';
-"#
-    )
-        .fetch_one(sql.get_ref())
-        .await
-        .map_err(|e| error::ErrorBadRequest(e))
-        .and_then(|v| Ok((
-            v
-                .try_get::<&str, _>(0)
-                .map(map_path(cfg.get_ref()))
-                .map_err(|e| error::ErrorBadRequest(e))?,
-            v
-                .try_get::<DateTime<Utc>, _>(1)
-                .map(|dt| dt.to_rfc3339_opts(SecondsFormat::Secs, true))
-                .map_err(|e| error::ErrorBadRequest(e))?
-            ))
-        )?;
-    let (fd, fd_dt) = sqlx::query(
-r#"
-SELECT file_name, start_dt
-FROM goesimg.files
-WHERE start_dt=(SELECT max(start_dt) FROM goesimg.files WHERE sector='FULL_DISK') AND sector='FULL_DISK';
-"#
-    )
-    .fetch_one(sql.get_ref())
-    .await
-    .map_err(|e| error::ErrorBadRequest(e.to_string()))
-    .and_then(|v| Ok((
-            v
-                .try_get::<&str, _>(0)
-                .map(map_path(cfg.get_ref()))
-                .map_err(|e| error::ErrorBadRequest(e))?,
-            v
-                .try_get::<DateTime<Utc>, _>(1)
-                .map(|dt| dt.to_rfc3339_opts(SecondsFormat::Secs, true))
-                .map_err(|e| error::ErrorBadRequest(e))?
-            ))
-        )?;
-}*/
