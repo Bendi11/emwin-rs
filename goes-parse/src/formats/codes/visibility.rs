2459c5d631d05f10c469161d35ca78145e6b6fd1
Bendi11 <bkliebmann@gmail.com>
2022-12-07 18:11:35 -0800

    Clean horizontal visibility code and allow fractional meter measurements

--- goes-parse/src/formats/codes/visibility.rs
+++ goes-parse/src/formats/codes/visibility.rs
@@ -7,7 +7,7 @@ use nom::{
     },
     combinator::{map_res, opt, eof, complete},
     error::context,
-    sequence::{preceded, terminated, tuple}, Parser, branch::alt,
+    sequence::{preceded, terminated, tuple, separated_pair}, Parser, branch::alt,
 };
 use nom_supreme::tag::complete::tag;
 use uom::si::{
@@ -19,55 +19,39 @@ use crate::ParseResult;
 
 /// Parse a surface horizontal visibility in `VVVV` format (pg. 227)
 pub fn vvvv(input: &str) -> ParseResult<&str, Length> {
-    let mut vis_sm = complete(context(
-        "horizontal visibility",
-        terminated(
+    fn fraction(input: &str) -> ParseResult<&str, f32> {
+        separated_pair(
+            map_res(digit1, |s: &str| s.parse::<f32>()),
+            char('/'),
+            map_res(digit1, |s: &str| s.parse::<f32>()),
+        )
+            .map(|(num, den)| num / den)
+            .parse(input)
+    }
+    
+    let (input, ignore) = complete(opt(tag("P6SM")))(input)?;
+    if ignore.is_some() {
+        return Ok((input, Length::new::<mile>(6f32)))
+    }
+
+    let (input, first) = complete(context(
+        "horizontal visibility first term",
+        alt((
+            fraction,
             tuple((
                 map_res(digit1, |s: &str| s.parse::<f32>()),
                 opt(preceded(
-                    char('/'),
-                    map_res(digit1, |s: &str| s.parse::<f32>()),
-                )),
-            )),
-            tag("SM"),
-        ),
-    ));
-
-    enum VisFirst {
-        Number(f32),
-        SM(f32),
-    }
-
-    let (input, vis_first) = complete(context(
-        "cloud visibility",
-        preceded(
-            space0,
-            alt((
-                map_res(&mut vis_sm, |(first, denominator)| {
-                    Ok::<VisFirst, ParseFloatError>(match denominator {
-                        Some(d) => VisFirst::SM(first / d),
-                        None => VisFirst::SM(first),
-                    })
-                }),
-                map_res(digit1, |s: &str| {
-                    Ok::<VisFirst, ParseFloatError>(VisFirst::Number(s.parse::<f32>()?))
-                }),
-                map_res(tag("P6SM"), |_| {
-                    Ok::<_, ParseFloatError>(VisFirst::SM(6f32))
-                }),
-            )),
-        ),
+                    space0,
+                    fraction,
+                ))
+            )).map(|(whole, frac)| whole + frac.unwrap_or(0f32)),
+        )) 
     ))(input)?;
 
-    Ok(match vis_first {
-        VisFirst::Number(whole) => match opt(preceded(space0, vis_sm))(input)? {
-            (input, Some((numerator, Some(denominator)))) => {
-                (input, Length::new::<mile>(whole + numerator / denominator))
-            }
-            (input, Some((numerator, None))) => (input, Length::new::<mile>(whole + numerator)),
-            (input, None) => (input, Length::new::<meter>(whole)),
-        },
-        VisFirst::SM(vis) => (input, Length::new::<mile>(vis)),
+    let (input, miles) = complete(opt(tag("SM")))(input)?;
+    Ok(match miles.is_some() {
+        true => (input, Length::new::<mile>(first)),
+        false => (input, Length::new::<meter>(first)),
     })
 }
 
